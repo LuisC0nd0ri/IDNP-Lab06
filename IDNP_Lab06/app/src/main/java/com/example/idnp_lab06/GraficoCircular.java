831fa5f242034873ec06067b8e2fccdfd704a685
Luis Condori <lcondorivill@unsa.edu.pe>
2022-11-21 15:38:22 -0500

    Teminando ejercicios

---
+++ IDNP_Lab06/app/src/main/java/com/example/idnp_lab06/GraficoCircular.java
@@ -0,0 +1,28 @@
+package com.example.idnp_lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GraficoCircular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafico_circular);
+
+        Button btnGraficoCircular;
+        btnGraficoCircular = (Button) findViewById(R.id.buttonAtrasGraficoCircular);
+
+        btnGraficoCircular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
